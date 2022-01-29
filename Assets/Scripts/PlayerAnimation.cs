5b45d43544bd4e2e06d4e51722ca861def85ee6e
darthvader99999 <bhow245@gmail.com>
2022-01-29 03:57:48 -0800

    New Animations and rotation code

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,17 +6,15 @@ public class PlayerAnimation : MonoBehaviour
 {
     private InputActions _inputActions;
     private Vector2 _prevDirection;
-    private bool _attacking;
-    private int _attackFrame;
-    public int attackFrameCount;
+    public bool Attacking;
     public PlayerReferences PlayerReferences;
+    public SpriteRenderer Renderer;
     public Animator Animator;
-
     void Start()
     {
         _inputActions = PlayerReferences.InputActions;
         _inputActions.Player.Movement.performed += OnMovement;
-        _inputActions.Player.Movement.canceled += OnMovement;   
+        _inputActions.Player.Movement.canceled += OnMovement;
     }
 
     // Update is called once per frame
@@ -23,62 +22,71 @@ public class PlayerAnimation : MonoBehaviour
     {
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-            _attacking = true;
-            _attackFrame = 0;
             Animator.SetTrigger("ForwardAttack");
         }
 
-        if (_attacking)
+        if (Mouse.current.leftButton.wasReleasedThisFrame)
         {
-            if (_attackFrame >= attackFrameCount)
-            {
-                _attacking = false;
-            }
-            else
-            {
-                _attackFrame++;
-            }
+            Animator.SetTrigger("Release");
         }
     }
     
     private void OnMovement(InputAction.CallbackContext callbackContext)
     {
-        if (_attacking)
+        if (Attacking)
         {
             return;
         }
         if (callbackContext.performed)
         {
-            Vector2 direction = callbackContext.ReadValue<Vector2>();
-            if (!direction.Equals(Vector2.zero))
-            {
-                if (direction.x > 0 && direction.x > _prevDirection.x)
-                {
-                    transform.rotation = Quaternion.Euler(0, 0, 0);
-                    Animator.SetTrigger("SideWalk");
-                }
-                else if (direction.x < 0 && direction.x < _prevDirection.x)
-                {
-                    transform.rotation = Quaternion.Euler(0, 180, 0);
-                    Animator.SetTrigger("SideWalk");
-                }
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            Vector2 direction = (mousePos-(Vector2)transform.position).normalized;
+            float angle = Vector2.Angle(new Vector2(1,0), direction);
 
-                if (direction.y > 0 && direction.y > _prevDirection.y)
+            if (angle <= 180 && angle > 135)
+            {
+                Animator.SetTrigger("SideWalk");
+                Renderer.flipY = true;
+                Renderer.flipX = false;
+                if (direction.y < 0)
+                {
+                    angle = -angle;
+                }
+            }
+            else if (angle <= 45 && angle > 0)
+            {
+                Animator.SetTrigger("SideWalk");
+                Renderer.flipY = false;
+                Renderer.flipX = false;
+                if (direction.y < 0)
+                {
+                    angle = -angle;
+                }
+            }else if (angle <= 135 && angle > 45)
+            {
+                if (direction.y > 0)
                 {
                     Animator.SetTrigger("BackwardWalk");
+                    Renderer.flipX = false;
+                    Renderer.flipY = false;
                 }
-                else if (direction.y < 0 && direction.y < _prevDirection.y)
+                else
                 {
                     Animator.SetTrigger("ForwardWalk");
+                    Renderer.flipX = true;
+                    Renderer.flipY = false;
                 }
-                Animator.ResetTrigger("Idle");
-                _prevDirection = direction;
+                angle += 270;
+                
+                Renderer.flipY = false;
             }
+            transform.rotation = Quaternion.Euler(0,0,angle);
+            Animator.ResetTrigger("Idle");
         }
         if(callbackContext.canceled)
         {
+            Animator.ResetTrigger("Idle");
             Animator.SetTrigger("Idle");
-            _prevDirection = Vector2.zero;
         }
     }
 
